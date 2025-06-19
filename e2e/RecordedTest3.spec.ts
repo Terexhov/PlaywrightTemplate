4d5d638c946b11cb749804b1a1fcbeadc6a3dd5d
Denis Terekhov <denisterekhov@MacBook-Air-Denis.local>
2025-06-19 13:21:03 +0300

    feat: успешные e2e тесты 2 и 3

--- e2e/RecordedTest3.spec.ts
+++ e2e/RecordedTest3.spec.ts
@@ -8,14 +8,13 @@ test('Создание новой роли', async ({ page }) => {
   await pom.login('testadmin@example.ru', '0ds9fpFj4efkgqd');
 
   // Переход к ролям
-  await pom.navigateToSection('Роли');
+  await page.getByRole('button').filter({ hasText: /^$/ }).click();
+  await page.getByText('Роли').click();
   await page.getByRole('complementary').getByRole('button').click();
-
+  
   // Заполнение данных роли
-  await page.getByRole('textbox', { name: 'Название' }).click();
-  await page.getByRole('textbox', { name: 'Название' }).fill('Тестовая роль');
-  await page.getByRole('textbox', { name: 'Описание' }).click();
-  await page.getByRole('textbox', { name: 'Описание' }).fill('Описание тестовой роли');
+  await page.getByRole('textbox', { name: 'Название роли' }).click();
+  await page.getByRole('textbox', { name: 'Название роли' }).type('Тестовая роль');
 
   // Сохранение роли
   await page.getByRole('complementary').getByRole('button', { name: 'Добавить' }).click();
